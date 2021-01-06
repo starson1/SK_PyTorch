6786a0854a40dbf3b534a0e3f0224437c7db0e94
vared <starson1234@gmail.com>
2021-01-06 09:48:06 +0900

    Lecture 5 practice code

--- LEC05/no5.py
+++ LEC05/no5.py
@@ -8,47 +8,31 @@ y_data = tensor([[2.0], [4.0], [6.0]])
 
 class Model(nn.Module):
     def __init__(self):
-        """
-        In the constructor we instantiate two nn.Linear module
-        """
-        super(Model, self).__init__()
-        self.linear = torch.nn.Linear(1, 1)  # One in and one out
+        super(Model, self).__init__() # super : 부모 메소드 상속.
+        self.linear = torch.nn.Linear(1, 1) #입력차원, 출력차원을 인수로 받음
 
     def forward(self, x):
-        """
-        In the forward function we accept a Variable of input data and we must return
-        a Variable of output data. We can use Modules defined in the constructor as
-        well as arbitrary operators on Variables.
-        """
         y_pred = self.linear(x)
         return y_pred
 
-
-# our model
 model = Model()
 
-# Construct our loss function and an Optimizer. The call to model.parameters()
-# in the SGD constructor will contain the learnable parameters of the two
-# nn.Linear modules which are members of the model.
 criterion = torch.nn.MSELoss(reduction='sum')
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+print(list(model.parameters()))
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01) # lr : 학습률
 
 # Training loop
 for epoch in range(500):
-    # 1) Forward pass: Compute predicted y by passing x to the model
     y_pred = model(x_data)
 
-    # 2) Compute and print loss
     loss = criterion(y_pred, y_data)
     print(f'Epoch: {epoch} | Loss: {loss.item()} ')
 
-    # Zero gradients, perform a backward pass, and update the weights.
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
 
 
-# After training
 hour_var = tensor([[4.0]])
 y_pred = model(hour_var)
 print("Prediction (after training)",  4, model(hour_var).data[0][0].item())
